56fad65f6df982347637a4da1b517607bce692c7
Jérémy Gallet <jeremy.gallet-ext@ixxitechside.com>
2025-06-16 16:56:50 +0200

    product / productType

---
+++ src/main/java/com/client/paris/Product.java
@@ -0,0 +1,3 @@
+package com.client.paris;
+
+public record Product(ProductType type, int quantity) {}
---
+++ src/main/java/com/client/paris/ProductType.java
@@ -0,0 +1,7 @@
+package com.client.paris;
+
+public enum ProductType {
+    TEL_HAUT_DE_GAMME,
+    TEL_MOYENNE_GAMME,
+    LAPTOP
+}
